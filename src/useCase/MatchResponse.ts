8ef1cd34166726e8bcfe9ab1e44c7961e99e2d3a
Gustavo Silva <gustavost1y@gmail.com>
2023-02-06 09:42:47 -0300

    feat: optmized match

--- src/useCase/MatchResponse.ts
+++ src/useCase/MatchResponse.ts
@@ -10,64 +10,119 @@ class MatchResponse {
         const currentPetId = Number(petid)
 
 
-
-        const isMatch = await prisma.match.findFirst({
-            where: {
-                OR: [
-                    { petId1: { equals: currentPetId } },
-                    { petId1: { equals: petMatchId } }
-                ],
-                AND: [
-                    {
-                        OR: [
-                            { petId2: { equals: currentPetId } },
-                            { petId2: { equals: petMatchId } }
-
-                        ]
-                    }
-                ]
-            }
-        })
-
-
-        if (isMatch) {
-
-            if (isMatch.ownerId1 == Number(req.userId) && isMatch.ownerInterest1) {
-                return res.status(200).json({
-                    message: "already sent"
-                })
-            } else if (
-                isMatch.ownerId2 == Number(req.userId) && isMatch.ownerInterest2
-            ) {
-                return res.status(200).json({
-                    message: "already sent"
-                })
+        try {
+            if (currentPetId == Number(petid)) {
+                throw new Error('You cannot match your own pet')
             }
 
 
-
-        } else {
-
-            const pet = await prisma.pet.findFirst({
+            const isMatch = await prisma.match.findFirst({
                 where: {
-                    id: petMatchId
+                    OR: [
+                        { petId1: { equals: currentPetId } },
+                        { petId1: { equals: petMatchId } }
+                    ],
+                    AND: [
+                        {
+                            OR: [
+                                { petId2: { equals: currentPetId } },
+                                { petId2: { equals: petMatchId } }
+
+                            ]
+                        }
+                    ]
                 }
             })
 
-            console.log(pet)
 
-            const createdMatch = await prisma.match.create({
-                data: {
-                    petId1: Number(petid),
-                    petId2: petMatchId,
-                    ownerId1: Number(req.userId),
-                    ownerId2: pet?.ownerId,
-                    ownerInterest1: match
+            if (isMatch) {
+
+                if (isMatch.ownerId1 == Number(req.userId) && isMatch.ownerInterest1 != null) {
+                    return res.status(200).json({
+                        message: "already sent"
+                    })
+                } else if (
+                    isMatch.ownerId2 == Number(req.userId) && isMatch.ownerInterest2 != null
+                ) {
+                    return res.status(200).json({
+                        message: "already sent"
+                    })
+                } else if (isMatch.ownerId1) {
+
+                    const nMatch = await prisma.match.update({
+                        where: {
+                            petId1_petId2: {
+                                petId1: isMatch.petId1,
+                                petId2: isMatch.petId2
+                            }
+                        },
+                        data: {
+                            ownerId2: Number(req.userId),
+                            ownerInterest2: match
+                        }
+                    })
+
+
+                    if (nMatch.ownerInterest1 == true && nMatch.ownerInterest2 == true) {
+                        return res.status(200).json({
+                            message: "IT'S A MATCH!!!"
+                        })
+                    } else if (nMatch.ownerInterest1 == true && nMatch.ownerInterest2 == false) {
+                        return res.status(200).json({
+                            message: "You missed a match"
+                        })
+                    }
+
+
+
+                } else if (isMatch.ownerId2) {
+
+                    const nMatch = await prisma.match.update({
+                        where: {
+                            petId1_petId2: {
+                                petId1: isMatch.petId1,
+                                petId2: isMatch.petId2
+                            }
+                        },
+                        data: {
+                            ownerId1: Number(req.userId),
+                            ownerInterest1: match
+                        }
+                    })
+
+                    return res.status(200).json({
+                        nMatch
+                    })
+
                 }
-            })
-            return res.send({ message: 'Ok' })
+
+
+
+            } else {
+
+                const pet = await prisma.pet.findFirst({
+                    where: {
+                        id: petMatchId
+                    }
+                })
+
+                console.log(pet)
+
+                const createdMatch = await prisma.match.create({
+                    data: {
+                        petId1: Number(petid),
+                        petId2: petMatchId,
+                        ownerId1: Number(req.userId),
+                        ownerId2: pet?.ownerId,
+                        ownerInterest1: match
+                    }
+                })
+                return res.send({ message: 'Ok' })
+            }
+
+        } catch (err: any) {
+            return res.status(400).json({ message: err.message })
         }
-
     }
 }
 
